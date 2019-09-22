5ae3962950f5dd2e133991ca3283df171e3dbeb4
tony <tony92151@gmail.com>
2019-09-22 14:46:19 +0800

    layer faild

--- layers/convolution_layer.hpp
+++ layers/convolution_layer.hpp
@@ -6,9 +6,11 @@
 #include <iostream>
 #include <cmath>
 #include <cassert>
+#include <thread>
 
 #define DEBUG false
 #define DEBUG_PREFIX "[DEBUG CONV LAYER ]\t"
+
 
 class ConvolutionLayer
 {
@@ -63,6 +65,33 @@ class ConvolutionLayer
 #endif
   }
 
+  std::vector<std::thread> threads;
+  //arma::cube outputT;
+
+  static void ConvolutionThread(size_t fidx, 
+                                arma::cube& outputT,
+                                arma::cube& input,
+                                std::vector<arma::cube> filters,
+                                size_t inputHeight,
+                                size_t inputWidth,
+                                size_t inputDepth,
+                                size_t filterHeight,
+                                size_t filterWidth,
+                                size_t verticalStride,
+                                size_t horizontalStride){
+    for (size_t i=0; i <= inputHeight - filterHeight; i += verticalStride){
+        for (size_t j=0; j <= inputWidth - filterWidth; j += horizontalStride){
+           outputT((i/verticalStride), (j/horizontalStride), fidx) = arma::dot(
+              arma::vectorise(
+                  input.subcube(i, j, 0,
+                                i+filterHeight-1, j+filterWidth-1, inputDepth-1)
+                ),
+              arma::vectorise(filters[fidx]));
+        }
+         
+      }
+  }
+
   void Forward(arma::cube& input, arma::cube& output)
   {
     // The filter dimensions and strides must satisfy some contraints for
@@ -71,26 +100,58 @@ class ConvolutionLayer
     assert((inputWidth - filterWidth)%horizontalStride == 0);
 
     // Output initialization.
-    output = arma::zeros((inputHeight - filterHeight)/verticalStride + 1,
+    arma::cube outputT = arma::zeros((inputHeight - filterHeight)/verticalStride + 1,
                          (inputWidth - filterWidth)/horizontalStride + 1,
                          numFilters);
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
+    
+    for(size_t fidx = 0; fidx < numFilters; fidx++){
+        threads.push_back(std::thread(ConvolutionThread, 
+                                            fidx,
+                                            input, 
+                                            filters,             
+                                            outputT, 
+                                            inputHeight, 
+                                            inputWidth, 
+                                            inputDepth,
+                                            filterHeight, 
+                                            filterWidth, 
+                                            verticalStride,
+                                            horizontalStride));
+    } 
+
+    for(int i = 0; i < threads.size(); i++){
+        //cout << threads[i].get_id() << endl;
+        threads[i].join();
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    
 
     // Perform convolution for each filter.
-    for (size_t fidx = 0; fidx < numFilters; fidx++)
-    {
-      for (size_t i=0; i <= inputHeight - filterHeight; i += verticalStride)
-        for (size_t j=0; j <= inputWidth - filterWidth; j += horizontalStride)
-          output((i/verticalStride), (j/horizontalStride), fidx) = arma::dot(
-              arma::vectorise(
-                  input.subcube(i, j, 0,
-                                i+filterHeight-1, j+filterWidth-1, inputDepth-1)
-                ),
-              arma::vectorise(filters[fidx]));
-    }
+    // for (size_t fidx = 0; fidx < numFilters; fidx++)
+    // {
+    //   for (size_t i=0; i <= inputHeight - filterHeight; i += verticalStride){
+    //     for (size_t j=0; j <= inputWidth - filterWidth; j += horizontalStride){
+    //        output((i/verticalStride), (j/horizontalStride), fidx) = arma::dot(
+    //           arma::vectorise(
+    //               input.subcube(i, j, 0,
+    //                             i+filterHeight-1, j+filterWidth-1, inputDepth-1)
+    //             ),
+    //           arma::vectorise(filters[fidx]));
+    //     }
+         
+    //   }
+        
+    // }
 
     // Store the input and output. This will be needed by the backward pass.
     this->input = input;
-    this->output = output;
+    this->output = outputT;
 
 #if DEBUG
     std::cout
